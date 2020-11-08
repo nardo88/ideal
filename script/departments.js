2cea5d74aa7d8d295f95919fbcb725c01b1e8e8e
nardo88 <nardo1988@mail.ru>
2020-11-08 14:39:59 +0300

    media

--- script/departments.js
+++ script/departments.js
@@ -1,18 +1,52 @@
 const departments = () => {
+    const sliderTopItem = document.querySelectorAll('.slider-top__item')
+    const tabsItem = document.querySelectorAll('.tabs__item')
+
+
     let departmentsSlider = new Swiper('.slider-top', {
-        loop: true,
         slideClass: 'slider-top__item',
         wrapperClass: 'slider-top__wrapper',
-        slidesPerView : 3,
+        slidesPerView: 3,
         spaceBetween: 30,
-        
+        navigation: {
+            nextEl: '.departments__next',
+            prevEl: '.departments__prev',
+        },
         pagination: {
             el: '.departments__pagination',
             clickable: true,
             type: 'bullets'
-            
         },
-       
+        breakpoints: {
+            
+            993: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+
+
+            },
+            640: {
+                slidesPerView: 2,
+            },
+            320: {
+                spaceBetween: 20,
+                slidesPerView: 1,
+            }
+
+        }
+    })
+
+    sliderTopItem.forEach((item, i) => {
+        item.addEventListener('click',() => {
+            tabsItem.forEach(item => {
+                item.classList.remove('active-tab')
+            })
+            sliderTopItem.forEach(slide => {
+                slide.classList.remove('active-slide')
+            })
+            tabsItem[i].classList.add('active-tab')
+            sliderTopItem[i].classList.add('active-slide')
+        })
     })
 }
 
